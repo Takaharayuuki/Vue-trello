8b8c2e0e36e9336da025417ae64f04f7d22349be
Takaharayuuki <yuuki.barber@gmail.com>
2021-02-16 10:51:45 +0900

    Add: ListAddコンポーネント

--- src/components/ListAdd.vue
+++ src/components/ListAdd.vue
@@ -1,11 +1,19 @@
 <template>
-  <form action="addlist" @submit.prevent="addList">
+  <form :class="classList" action="addlist" @submit.prevent="addList">
     <input
       v-model="title"
       type="text"
       class="text-input"
-      placeholder="Add new list">
-    <button type="submit" class="add-button">
+      placeholder="Add new list"
+      @focusin="startEditing"
+      @focusout="finishEditing"
+      >
+    <button
+      type="submit"
+      :class="titleExists"
+      class="add-button"
+      v-if="isEditing || titleExists"
+      >
       Add
     </button>
   </form>
@@ -17,17 +25,28 @@ export default {
   data () {
     return {
       title: '',
+      isEditing: false,
     }
   },
   methods: {
     addList() {
       this.$store.dispatch('addlist', { title: this.title })
       this.title = ''
+    },
+    startEditing() {
+      this.isEditing = true
+    },
+    finishEditing() {
+      this.isEditing = false
     }
+  },
+  computed: {
+    classList() {
+      return this.isEditing ? 'active' : ''
+    },
+    titleExists() {
+      return this.title.length > 0 ? 'addable' : ''
+    },
   }
 }
 </script>
-
-<style>
-
-</style>
