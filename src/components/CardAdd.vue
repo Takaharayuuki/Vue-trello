18958f0ba02b1b5c1fe5c29e0d3c66c5a1018141
Takaharayuuki <yuuki.barber@gmail.com>
2021-02-16 11:53:22 +0900

    add: forcusin out 実装

--- src/components/CardAdd.vue
+++ src/components/CardAdd.vue
@@ -1,11 +1,17 @@
 <template>
-  <form class="addcard" @submit.prevent="addCardToList">
+  <form :class="classList" class="addcard" @submit.prevent="addCardToList">
     <input v-model="body"
-           type="text"
-           class="text-input"
-           placeholder="Add new card"
+      type="text"
+      class="text-input"
+      placeholder="Add new card"
+      @focusin="startEditing"
+      @focusout="finishEditing"
     />
-    <button type="submit" class="add-button">
+    <button
+      type="submit"
+      :class="titleExists"
+      class="add-button"
+      v-if="isEditing || titleExists">
       Add
     </button>
   </form>
@@ -23,14 +29,29 @@ export default {
   data() {
     return {
       body: '',
+      isEditing: false,
     }
   },
   methods: {
     addCardToList() {
-      this.$store.dispatch('addCardList', { body: this.body, listIndex: this.listIndex })
+      this.$store.dispatch('addCardToList', { body: this.body, listIndex: this.listIndex })
       this.body = ''
+    },
+    startEditing() {
+      this.isEditing = true
+    },
+    finishEditing() {
+      this.isEditing = false
     }
   },
+  computed: {
+    classList() {
+      return this.isEditing ? 'active' : ''
+    },
+    titleExists() {
+      return this.body.length > 0 ? 'addable' : ''
+    },
+  }
 }
 </script>
 
